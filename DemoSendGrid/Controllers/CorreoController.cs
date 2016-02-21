8f34a92fe96ca623baa962050efb0768cdc5ef9e
Daniel Corregidor Coronado <corregidor10@hotmail.com>
2016-02-21 22:45:14 +0100

    1ra Subida

---
+++ DemoSendGrid/Controllers/CorreoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoSendGrid.Controllers
+{
+    public class CorreoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult EnviarCorreo(String Destino, String Asunto, String Mensaje)
+        {
+            
+            new EnviarCorreos().PostMails(Destino, Asunto, Mensaje);
+
+            return View("Index");
+        }
+        
+       
+    }
+}
---
+++ DemoSendGrid/EnviarCorreos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Web;
+
+namespace DemoSendGrid
+{
+    public class EnviarCorreos
+    {
+        public void PostMails(String mailsender, String asunto, String body)
+        {
+            try
+            {
+                MailMessage mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress(mailsender));
+
+                // From
+                mailMsg.From = new MailAddress("info@encuestator.com", "Soy yo, Tonto");
+
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = asunto;
+                string text = body;
+                string html = body;
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+              
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+             
+
+                smtpClient.Send(mailMsg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
